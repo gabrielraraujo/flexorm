c02f3f7bcaf1f08316d0b350e37dad4bedb1110d
Gabriel Ribeiro <gabrielribeiro@wesistemas.net.br>
2012-08-11 11:58:43 -0400

    Commit

---
+++ Flex/Database/DatabaseConnectionAbstract.php
@@ -0,0 +1,14 @@
+<?php
+
+	namespace Flex\Database;
+
+	class DatabaseConnectionAbstract
+	{
+		
+		private $dsn = "";
+		private $server = "";
+		private $username = "";
+		private $port = 3306;
+		private $password = "";
+		
+	}
